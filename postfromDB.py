f0bde9397e74271241463249060b074f91fd0af7
ttay <ttay@umich.edu>
2018-08-15 16:06:14 +0800

    added the git module

--- postfromDB.py
+++ postfromDB.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import os
 import time
 import configparser
 import sqlite3
+from datetime import datetime
 from InstagramAPI import InstagramAPI
+
 
 """
 Posts automatically based on the posts_db database
@@ -14,6 +17,7 @@ If there is, it posts that post, prints a helpful message to the screen, then re
 If not, it prints a helpful error message and then returns false
 """
 
+#Initialization
 config = configparser.ConfigParser()
 config.read("login.ini")
 username = config["LOGIN"]["username"]
@@ -23,20 +27,31 @@ posts_db = sqlite3.connect(posts_db_name, timeout=0,
                                 isolation_level=None) # added a posts database
 posts_db_c = posts_db.cursor()
 
+#Check the Posts database and select a file
 posts_db_c.execute('SELECT filename, title, tags FROM posts WHERE post_date IS NULL')
 toPost = posts_db_c.fetchone()
+if toPost is None:
+    print ("No more posts left in the database. Please update.")
+    raise SystemExit(1)
+
 filename = "/home/thomas/insta-bot/img/" + toPost[0]
 title = toPost[1]
+
 #Handle tags
 tagList = toPost[2].split(", ")
 tagStr = '#' + " #".join(tagList)
 captionStr = "{0} {1}".format(title, tagStr)
 print ("Found image at: {0}\nCaption: {1}".format(filename, captionStr))
+
 #Login using Instagram API
 api = InstagramAPI(username, passwd)
 api.login()  # login
 isUploaded = api.uploadPhoto(filename, caption="This is real stuff #truth #deep")
-posts_db_c.execute("UPDATE posts SET post_date = '2018-08-14' WHERE filename = '{0}'".format(toPost[0]))
+
+today = datetime.today()
+todayStr = today.strftime("%Y-%m-%d %H:%M")
+posts_db_c.execute("UPDATE posts SET post_date = '{1}' WHERE filename = '{0}'".format(toPost[0], todayStr)
+                  )
 
 #Close
 posts_db_c.close()
