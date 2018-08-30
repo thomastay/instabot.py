0cff151e1927b0e15062cab2c8eec1980591567a
ttay <ttay@umich.edu>
2018-08-30 23:06:35 +0800

    Updated the scheduler to be Object Oriented
    Wrapped the postbot in a class to be called by the server

--- postbot.py
+++ postbot.py
@@ -7,63 +7,67 @@ import configparser
 import sqlite3
 from datetime import datetime
 from InstagramAPI import InstagramAPI
+from InstagramAPI.exceptions import SentryBlockException
 
+class Postbot():
+    def __init__(self, username, password, posts_db_name = "posts_db.db"):
+        self.status = "Not posting"
+        self.username = username
+        self.password = password
+        self.posts_db_name = posts_db_name
+        self.posts_db = sqlite3.connect(posts_db_name, timeout=0,
+                                        isolation_level=None) # added a posts database
+        self.posts_db_c = self.posts_db.cursor()
 
-
-def post():
-    """
-    Posts automatically based on the posts_db database
-    First reads the posts database and checks whether there is a post that
-    hasn't been opened.
-    If there is, it posts that post, prints a helpful message to the screen, then returns true
-    If not, it prints a helpful error message and then returns false
-    """
-#Initialization
-    config = configparser.ConfigParser()
-    config.read("login.ini")
-    username = config["LOGIN"]["username"]
-    passwd = config["LOGIN"]["password"]
-    posts_db_name = "posts_db.db"
-    posts_db = sqlite3.connect(posts_db_name, timeout=0,
-                                    isolation_level=None) # added a posts database
-    posts_db_c = posts_db.cursor()
-
+    def post(self):
+        """
+        Posts automatically based on the posts_db database
+        First reads the posts database and checks whether there is a post that
+        hasn't been opened.
+        If there is, it posts that post, prints a helpful message to the screen, then returns true
+        If not, it prints a helpful error message and then returns false
+        """
+        self.status = "Posting"
 #Check the Posts database and select a file
-    posts_db_c.execute('SELECT filename, title, tags FROM posts WHERE post_date IS NULL')
-    toPost = posts_db_c.fetchone()
-    if toPost is None:
-        print ("Postbot: No more posts left in the database. Please update.")
-        return False
+        self.posts_db_c.execute('SELECT filename, title, tags FROM posts WHERE post_date IS NULL')
+        self.toPost = self.posts_db_c.fetchone()
+        if self.toPost is None:
+            print ("Postbot: No more posts left in the database. Please update.")
+            return False
 
-    filename = "/home/thomas/insta-bot/img/" + toPost[0]
-    title = toPost[1]
+        self.filename = "/home/thomas/insta-bot/img/" + self.toPost[0]
+        self.title = self.toPost[1]
 
 #Handle tags
-    tagList = toPost[2].split(", ")
-    tagStr = '#' + " #".join(tagList)
-    captionStr = "{0} {1}".format(title, tagStr)
-    print ("Postbot: Found image at: {0}\nCaption: {1}".format(filename, captionStr))
+        self.tagList = self.toPost[2].split(", ")
+        self.tagStr = '#' + " #".join(self.tagList)
+        self.captionStr = "{0} {1}".format(self.title, self.tagStr)
+        print ("Postbot: Found image at: {0}\nCaption: {1}".format(self.filename, self.captionStr))
 
 #Login using Instagram API
-    api = InstagramAPI(username, passwd)
-    print ("Postbot: ", end='')
-    api.login()  # login
-    print ("Postbot: ", end='')
-    isUploaded = api.uploadPhoto(filename, caption=captionStr)
-    print ("Postbot: Is Uploaded {0}".format(isUploaded))
-
-    today = datetime.today()
-    todayStr = today.strftime("%Y-%m-%d %H:%M")
-    posts_db_c.execute("UPDATE posts SET post_date = '{1}' WHERE filename = '{0}'".format(toPost[0], todayStr)
-                      )
+        self.api = InstagramAPI(self.username, self.passwd)
+        print ("Postbot: ", end='')
+        self.api.login()  # login
+        self.isUploaded = False
+        try:
+            self.isUploaded = self.api.uploadPhoto(self.filename, caption=self.captionStr)
+            print ("Postbot: {0}".format("Uploaded successfully" if self.isUploaded else "Upload failed"))
+            if self.isUploaded:
+                self.todayStr = datetime.today().strftime("%Y-%m-%d %H:%M")
+                self.posts_db_c.execute("UPDATE posts SET post_date = '{1}' WHERE filename = '{0}'".format(
+                    self.toPost[0], self.todayStr))
+        except (RuntimeError, SentryBlockException) as error:
+            print ("Unable to upload photo because of the following reasons:")
+            print (error)
+            #self.posts_db_c.execute("UPDATE posts SET post_date = \
+            #        '2000-00-00' WHERE filename = '{0}'".format(self.toPost[0]))
 
 #Close
-    posts_db_c.close()
-    posts_db.commit()
-    signal.signal(signal.SIGTERM, api.logout())
-    atexit.register(apit.logout())
-    return True
+        self.posts_db.commit()
+        return isUploaded
 
-if __name__ == "__main__":
-    post()
-
+    def stop(self):
+        self.posts_db_c.close()
+        self.posts_db.commit()
+        self.status = "Closed"
+        return False
--- scheduler.py
+++ scheduler.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env/python
 from likerbot import init_bot, run_bot
-from postbot import post
+from postbot import Postbot
 import time
 import threading
 import configparser
 
-def schedule_post(sleep_time = 5000):
+class Scheduler:
     """
-    Schedules a post
-    post() returns true if it posted a post, if not it returns false
+    Custom class which runs the bot
     """
-    while post():
-        print ("Postbot: Postbot is sleeping")
-        time.sleep(sleep_time)
+    def __init__(self):
+        self.status = "Not Running"
 
-def getDetails():
-    config = configparser.ConfigParser()
-    config.read("login.ini")
-    username = config["LOGIN"]["username"]
-    passwd = config["LOGIN"]["password"]
-    return (username, passwd)
+    def start(self):
+        self.getDetails() #Grabs passwords from login.ini
+        self.post_bot = Postbot(self.username, self.passwd)
+        self.post_bot_thread = threading.Thread(target = self.schedule_post)
+        self.liker_bot = init_bot(self.username, self.passwd)
+
+    def run(self):
+        run_bot(self.liker_bot)
+        self.post_bot_thread.start()
+
+    def stop(self):
+        self.liker_bot.logout()
+        self.post_bot_thread.join()
+
+    def schedule_post(self, sleep_time = 5000):
+        """
+        Schedules a post
+        post() returns true if it posted a post, if not it returns false
+        """
+        if self.post_bot is not None:
+            while self.post_bot.post():
+                print ("Postbot: Postbot is sleeping")
+                time.sleep(sleep_time)
+
+    def getDetails(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("login.ini")
+        self.username = self.config["LOGIN"]["username"]
+        self.passwd = self.config["LOGIN"]["password"]
 
 
 def main():
